06173849b7fdb36ddfb149409378d323c22407de
Roody <rjointer2@gmail.com>
2021-11-08 10:19:40 -0800

    createing and configing DB

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+
+// modules 
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import { ApolloServer } from 'apollo-server-express';
+
+// port 
+const _PORT = process.env.PORT || 1234;
+
+// Type Defs
+
+// Resolvers
+
+// Middleware
+
+
+// init new apollo server
+
+// connection to the DB
+
